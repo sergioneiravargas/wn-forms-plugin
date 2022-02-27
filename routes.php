dd7c5340af61b889c3424a84350aa0b9e5f3f61d
Sergio Neira Vargas <sh.neiravargas@gmail.com>
2022-02-27 19:33:56 -0300

    Update: API endpoints, plugin details

--- routes.php
+++ routes.php
@@ -1,49 +1,21 @@
 <?php
 
-use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Route;
 use Sntools\Forms\Models\Form;
-use Sntools\Forms\Models\Message;
-use Winter\Storm\Auth\Manager;
 
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: *');
 header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Key, Authorization');
 header('Access-Control-Allow-Credentials: true');
 
-Route::get('sntools/forms/api/forms', function () {
+Route::get('api/forms', function () {
     return Form::paginate(20);
 });
 
-Route::get('sntools/forms/api/forms/{id}', function ($id) {
+Route::get('api/forms/{id}', function ($id) {
     return Form::find($id);
 });
 
-Route::get('sntools/forms/api/forms/{id}/messages', function ($id) {
-    if (!Manager::instance()->getUser()) {
-        App::abort(403);
-    }
-
-    return Message::where('form_id', '=', $id)
-        ->paginate(20);
+Route::options('api/messages', function () {
 });
-
-Route::get('sntools/forms/api/messages', function () {
-    if (!Manager::instance()->getUser()) {
-        App::abort(403);
-    }
-
-    return Message::paginate(20);
-});
-
-Route::get('sntools/forms/api/messages/{id}', function ($id) {
-    if (!Manager::instance()->getUser()) {
-        App::abort(403);
-    }
-
-    return Message::find($id);
-});
-
-Route::options('sntools/forms/api/messages', function () {
-});
-Route::post('sntools/forms/api/messages', 'Sntools\Forms\Controllers\Messages@sendMessage');
+Route::post('api/messages', 'Sntools\Forms\Controllers\Messages@sendMessage');
